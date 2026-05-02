5dd0374e9a9d1030eb1bd5b144b18d3bb2122206
Samin Ahmed Chowdhury <samin.ahmedchowdhury@gmail.com>
2026-05-02 11:17:08 +0600

    dfdf

--- Mid/Exam_codes/RRPrito.cpp
+++ Mid/Exam_codes/RRPrito.cpp
@@ -3,93 +3,85 @@
 #include <algorithm>
 using namespace std;
 
-struct Process {
+struct Process
+{
     string pid;
     int at, bt, rt, pr;
     int ct, tat, wt;
 };
 
-int main() {
-
+int main()
+{
     int n;
     cin >> n;
-
     int tq;
     cin >> tq;
-
     Process p[n];
-
     // Input
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         cin >> p[i].pid >> p[i].at >> p[i].bt >> p[i].pr;
         p[i].rt = p[i].bt;
     }
-
     int time = 0, complete = 0;
-
     bool visited[n] = {false};
-
     string ganttPID[200];
     int ganttTime[200];
     int g = 0;
-
     ganttTime[g++] = 0;
-
-    while (complete < n) {
-
+    while (complete < n)
+    {
         // Find highest priority among arrived processes
         int best_pr = 9999;
-
-        for (int i = 0; i < n; i++) {
-            if (p[i].at <= time && p[i].rt > 0) {
+        for (int i = 0; i < n; i++)
+        {
+            if (p[i].at <= time && p[i].rt > 0)
+            {
                 best_pr = min(best_pr, p[i].pr);
             }
         }
-
         // Create queue for same priority (RR)
         queue<int> q;
-
-        for (int i = 0; i < n; i++) {
-            if (p[i].at <= time && p[i].rt > 0 && p[i].pr == best_pr && !visited[i]) {
+        for (int i = 0; i < n; i++)
+        {
+            if (p[i].at <= time && p[i].rt > 0 && p[i].pr == best_pr && !visited[i])
+            {
                 q.push(i);
                 visited[i] = true;
             }
         }
-
-        if (q.empty()) {
+        if (q.empty())
+        {
             time++;
             continue;
         }
-
-        while (!q.empty()) {
-
+        while (!q.empty())
+        {
             int i = q.front();
             q.pop();
-
-            ganttPID[g-1] = p[i].pid;
-
+            ganttPID[g - 1] = p[i].pid;
             int execTime = min(tq, p[i].rt);
-
             p[i].rt -= execTime;
             time += execTime;
-
             ganttTime[g] = time;
             g++;
-
             // Add newly arrived with same priority
-            for (int j = 0; j < n; j++) {
+            for (int j = 0; j < n; j++)
+            {
                 if (p[j].at <= time && p[j].rt > 0 &&
-                    p[j].pr == best_pr && !visited[j]) {
+                    p[j].pr == best_pr && !visited[j])
+                {
 
                     q.push(j);
                     visited[j] = true;
                 }
             }
-
-            if (p[i].rt > 0) {
+            if (p[i].rt > 0)
+            {
                 q.push(i);
             }
-            else {
+            else
+            {
                 complete++;
 
                 p[i].ct = time;
@@ -97,34 +89,31 @@ int main() {
                 p[i].wt = p[i].tat - p[i].bt;
             }
         }
-
         // Reset visited for next priority level
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++)
+        {
             if (p[i].rt > 0)
                 visited[i] = false;
         }
     }
-
     // -------- Gantt Chart --------
     cout << "\nGantt Chart:\n";
-
     cout << "| ";
-    for (int i = 0; i < g-1; i++) {
+    for (int i = 0; i < g - 1; i++)
+    {
         cout << ganttPID[i] << " | ";
     }
-
     cout << "\n0\t";
-    for (int i = 1; i < g; i++) {
+    for (int i = 1; i < g; i++)
+    {
         cout << ganttTime[i] << "\t";
     }
 
     // -------- Table --------
     float total_wt = 0, total_tat = 0;
-
     cout << "\n\nPID\tAT\tBT\tPR\tCT\tTAT\tWT\n";
-
-    for (int i = 0; i < n; i++) {
-
+    for (int i = 0; i < n; i++)
+    {
         cout << p[i].pid << "\t"
              << p[i].at << "\t"
              << p[i].bt << "\t"
@@ -136,13 +125,10 @@ int main() {
         total_wt += p[i].wt;
         total_tat += p[i].tat;
     }
-
     cout << "\nAverage Waiting Time: " << total_wt / n << endl;
     cout << "Average Turnaround Time: " << total_tat / n << endl;
-
     return 0;
-}   
-
+}
 
 /*
 4
